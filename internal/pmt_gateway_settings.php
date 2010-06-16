677ed3b91cc3f09da86e5535b8b4f615483a91a8
Chris Webster <wxguychris@gmail.com>
2010-06-16 11:17:49 -0400

    common file with gateway info in it

---
+++ internal/pmt_gateway_settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  This file contains the information to pass to the payment processor's gateway in order to process transactions properly
+ *
+ */
+$payment['GatewayID']='50414';
+$payment['language']='English';
+$payment['ReturnURL']='http://www.lgbtcenterofraleigh.com/thank-you.html';
+
+$payment['Products']='Price::Qty::Code::Description::Flags';
+
+?>
